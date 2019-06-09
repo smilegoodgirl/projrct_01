d5b7bdbd57773cc3f92ca419707f61512afd159b
gml <1957004795@qq.com>
2019-06-09 16:56:37 +0800

    Initial commit

---
+++ javaweb01/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: leglon
+  Date: 2019/6/8
+  Time: 23:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>test</title>
+  </head>
+  <body>
+  test
+  </body>
+</html>
---
+++ src/com/gml/java/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.gml.java;
+
+
+public class HelloWorld {
+
+    /*
+     * @方法名:
+     * @描述: //TODO 
+     * @参数: 
+     * @返回类型  
+     * @开发者: leglon
+     * @创建时间: 2019/6/9 15:46
+     **/
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("hhhh");
+
+    }
+  
+}
